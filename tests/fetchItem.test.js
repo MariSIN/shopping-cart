9bacd51ca7b361aa17e1a91c2e30d329303ed6c7
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-09-12 14:56:56 -0300

    inicia projeto

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -1,5 +1,6 @@
 require('../mocks/fetchSimulator');
 const { fetchItem } = require('../helpers/fetchItem');
+
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
