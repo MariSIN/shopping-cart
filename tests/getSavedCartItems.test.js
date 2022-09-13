8466d4e8f8d6392b7827bdb9889d8b05cc413230
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-09-13 19:19:46 -0300

    adiciona requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,6 +5,16 @@ const { expect } = require('@jest/globals');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('4.1 - Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado.',  () => {
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+
+  it('4.2 - Testa se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o `cartItems` como parâmetro.', () => {
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
+  });
 });
+
